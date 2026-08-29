97c2aefdad5e178faa2ccb9b796c0f2c5259ee6c
Utkarsh Tripathi <133485209+utk12rsh@users.noreply.github.com>
2026-08-29 17:21:32 +0530

    Time: 42 ms (5.13%), Space: 63 MB (78.63%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cs
@@ -0,0 +1,47 @@
+public class Solution {
+    public bool CanBouquetBePrepared(int[] bloomDay, int m, int k, int mid){
+        int bouquetCount = 0;
+        int flowerCount = 0;
+
+        for(int i = 0; i < bloomDay.Length; i++){
+            int perFlowerVal = (int) Math.Ceiling((double) bloomDay[i] / mid);
+
+            if(perFlowerVal == 1 && flowerCount < k){
+                flowerCount++;
+            }
+            else if(perFlowerVal > 1){
+                flowerCount = 0;
+            }
+
+            if(flowerCount == k && bouquetCount < m){
+                bouquetCount++;
+                flowerCount = 0;
+            }
+        }
+
+        return bouquetCount >= m;
+    }
+
+    public int MinDays(int[] bloomDay, int m, int k) {
+        if(m*k > bloomDay.Length){
+            return -1;
+        }
+        
+        int start = 1;
+        int end = bloomDay.Max();
+        int result = -1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(CanBouquetBePrepared(bloomDay, m, k, mid)){
+                result = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+}
