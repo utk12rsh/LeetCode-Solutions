1fc8d941a5ed8015fb43367c10e0add736ef93d7
Utkarsh Tripathi <133485209+utk12rsh@users.noreply.github.com>
2026-08-29 16:02:04 +0530

    Time: 9 ms (60.67%), Space: 49.2 MB (83.15%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public bool IsSmallestDivisor(int[] nums, int threshold, int mid){
+        int totalSum = 0;
+
+        for(int i = 0 ; i < nums.Length; i++){
+            int remainder = (int) Math.Ceiling((double) nums[i] / mid);
+            totalSum += remainder;
+        }
+
+        return totalSum <= threshold;
+    }
+
+    public int FindMaximum(int[] nums){
+        int maxValue = nums[0];
+
+        for(int i = 1; i < nums.Length; i++){
+            if(nums[i] > maxValue)
+                maxValue = nums[i];
+        }
+
+        return maxValue;
+    }
+
+    public int SmallestDivisor(int[] nums, int threshold) {
+        int start = 1;
+        int end = FindMaximum(nums);
+
+        while(start < end){
+            int mid = start + (end - start) / 2;
+
+            if(IsSmallestDivisor(nums, threshold, mid)){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return end;
+    }
+}
