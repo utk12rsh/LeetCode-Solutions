d3379997ca7a0cdc203c6499e091b03ceb670502
Utkarsh Tripathi <133485209+utk12rsh@users.noreply.github.com>
2026-08-29 09:40:43 +0530

    Time: 1 ms (52.15%), Space: 63.5 MB (75.56%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        int n = nums.Length;
+        k %= n;
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = result[i];
+        }
+    }
+}
