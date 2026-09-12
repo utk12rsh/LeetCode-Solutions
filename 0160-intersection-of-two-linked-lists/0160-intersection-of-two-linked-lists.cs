87c37eb4b586655e9728ead9156acbb2f25e0419
Utkarsh Tripathi <133485209+utk12rsh@users.noreply.github.com>
2026-09-12 17:05:16 +0530

    Time: 155 ms (94.93%), Space: 70.1 MB (41.94%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cs
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cs
@@ -7,17 +7,43 @@
  * }
  */
 public class Solution {
-public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
-{
-    ListNode p1 = headA;
-    ListNode p2 = headB;
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        int aLength = 0;
+        int bLength = 0;
 
-    while (p1 != p2)
-    {
-        p1 = (p1 == null) ? headB : p1.next;
-        p2 = (p2 == null) ? headA : p2.next;
+        ListNode temp = headA;
+        while (temp != null) 
+        {
+            aLength++;
+            temp = temp.next;
+        }
+
+        temp = headB;
+        while (temp != null)
+        {
+            bLength++;
+            temp = temp.next;
+        }
+
+        int diff = Math.Abs(aLength - bLength);
+
+        for (int i = 0; i < diff; i++)
+        {
+            if (bLength > aLength)
+                headB = headB.next;
+            else
+                headA = headA.next;
+        }
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b)
+        {
+            a = a.next;
+            b = b.next;
+        }
+
+        return a;
     }
-
-    return p1;
-}
 }
